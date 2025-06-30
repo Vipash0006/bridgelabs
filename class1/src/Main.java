2ebd67df1b5e34dfdbce03224d8dab25e036e089
Vipash0006 <vipasharora06@gmail.com>
2025-06-30 12:50:12 +0530

    Initial commit

---
+++ class1/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Pragati");
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
