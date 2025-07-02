81ec5565eea48db31ccd5249395927c1e7a7dfe9
Vipash0006 <122520488+Vipash0006@users.noreply.github.com>
2025-07-02 13:07:37 +0530

    Add files via upload

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,32 +1,47 @@
 import java.util.*;
 public class Main {
-
-    static void addition(int a,int b){
-        int sum = a+b;
-        System.out.println("the sum of a and b is: "+ sum);
-    }
-    static void subtraction(int a, int b){
-        int sub = a-b;
-        System.out.println("the sub of a and b is: "+sub);
-    }
-    static void multiplication(int a, int b){
-        int product = a*b;
-        System.out.println("the product of a and b is: "+product);
-    }
-    static void division(int a, int b){
-        int divide = a/b;
-        System.out.println("the division of a and b is: "+divide);
-    }
+//
+//    static void addition(int a,int b){
+//        int sum = a+b;
+//        System.out.println("the sum of a and b is: "+ sum);
+//    }
+//    static void subtraction(int a, int b){
+//        int sub = a-b;
+//        System.out.println("the sub of a and b is: "+sub);
+//    }
+//    static void multiplication(int a, int b){
+//        int product = a*b;
+//        System.out.println("the product of a and b is: "+product);
+//    }
+//    static void division(int a, int b){
+//        int divide = a/b;
+//        System.out.println("the division of a and b is: "+divide);
+//    }
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       System.out.print("Enter the variable a: ");
-       int a = sc.nextInt();
-        System.out.print("Enter the variable b: ");
-       int b = sc.nextInt();
-       addition(a,b);
-       subtraction(a,b);
-       multiplication(a,b);
-       division(a,b);
+//       System.out.print("Enter the variable a: ");
+//       int a = sc.nextInt();
+//        System.out.print("Enter the variable b: ");
+//       int b = sc.nextInt();
+//       addition(a,b);
+//       subtraction(a,b);
+//       multiplication(a,b);
+//       division(a,b);
+        float floatValue1 = 2.0f;
+        float floatValue2 = 4.0f;
+        float floatResult = floatValue1+floatValue2;
+
+        System.out.println("Float: "+floatResult);
+
+
+        double doubleValue1 = 2.0;
+        double doubleValue2 = 4.0;
+        double doubleResult = doubleValue1 + doubleValue2;
+
+        System.out.println("Double: "+doubleResult);
+
+
+
 
 
     }
