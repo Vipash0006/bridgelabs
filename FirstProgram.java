c62a670e02a86000e38d08738a4456b43f3b6b54
Vipash0006 <122520488+Vipash0006@users.noreply.github.com>
2025-07-02 12:45:41 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FirstProgram{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
