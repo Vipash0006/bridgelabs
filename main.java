62b43b00137c2f29385c6409d8b4f6877713115c
Vipash0006 <122520488+Vipash0006@users.noreply.github.com>
2025-07-03 11:34:42 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FirstProgram{
+
+    public static void main(String[] args){
+c
+        System.out.println("Hello");
+        System.out.println("Salmon_Bhai");
+        
+    }
+}
